60e5ef6117b4dec551363304ea6003219dbefc02
prathmesh-sargar <sargarprathmesh007@gmail.com>
2026-02-28 12:21:31 +0530

    flaskapp v1.1

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
@@ -15,5 +16,17 @@ def about():
 def contact():
     return render_template("contact.html")
 
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "ok",
+        "environment": os.environ.get("APP_ENV", "unknown")
+    }), 200
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+
+
+    # local running command : 
+    #  docker run --env-file .env -p 5000:5000 flaskapp
